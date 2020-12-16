e91527ed8c18bbf1a30dc45c323126c8b740c7c0
mingmuchuan <1871998048@qq.com>
2020-12-16 15:47:56 +0800

    vercel

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-12-16 15:44:38
+ * @LastEditTime: 2020-12-16 15:44:46
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /nuxt-vercel/pages/index.vue
+-->
+<template>
+  <div>index</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
