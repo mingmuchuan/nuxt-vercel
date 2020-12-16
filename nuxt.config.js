aac124bf1cfb6aa61abc52a9d0c4a514a47bacd0
mingmuchuan <1871998048@qq.com>
2020-12-16 15:53:36 +0800

    vercel-deploy

---
+++ nuxt.config.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2020-12-16 15:53:06
+ * @LastEditTime: 2020-12-16 15:53:14
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /nuxt-vercel/nuxt.config.js
+ */
+module.exports = {
+  ssr: true
+}
